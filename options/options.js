f1b17577bcdcf6c4da1dc14f1687904931d49f8b
Crowdbotics <team@crowdbotics.com>
2021-09-24 16:47:56 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "prexplay_30826",
+  url: "https://prexplay_30826.botics.co",
+  api: "https://prexplay_30826.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
